325c0c4ca51939f760dec37a862447bd1c54c6f3
Robin Norwood <rnorwood@hashicorp.com>
2021-09-28 10:24:01 -0500

    Update to Cassandra example

--- main.tf
+++ main.tf
@@ -4,70 +4,59 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 3.0"
     }
-    ## Add during tutorial:
-    awscc = {
-      source = "hashicorp/awscc"
-    }
+## Add during tutorial:
+#     awscc = {
+#       source  = "hashicorp/awscc"
+# #      version = "FIXME - set to version once known"
+#     }
   }
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region = var.aws_region
 }
 
 ## Add during tutorial:
-provider "awscc" {
-  region = "us-west-2"
-}
+# provider "awscc" {
+#   region = var.aws_region
+# }
 
-resource "aws_kms_key" "example" {
+resource "aws_kms_key" "terraform" {
   description = "Example key for Cassandra table"
 }
 
-# resource "aws_s3_bucket" "source" {
-#   bucket_prefix = "awscc-appflow-source-"
-#   force_destroy = true
-#   acl           = "private"
-# }
-
-# resource "aws_s3_bucket" "destination" {
-#   bucket_prefix = "awscc-appflow-destination-"
-#   force_destroy = true
-#   acl           = "private"
-# }
-
 ## Add during tutorial:
+# resource "awscc_cassandra_keyspace" "terraform" {
+#   keyspace_name = "terraform"
+# }
 
-resource "awscc_cassandra_keyspace" "example" {
-  keyspace_name = "terraform"
-}
+# resource "awscc_cassandra_table" "obbits" {
+#   keyspace_name = awscc_cassandra_keyspace.terraform.keyspace_name
+#   table_name    = "obbits"
 
-resource "awscc_cassandra_table" "example" {
-  keyspace_name = awscc_cassandra_keyspace.example.keyspace_name
+#   partition_key_columns = [
+#     {
+#       column_name : "id"
+#       column_type : "int"
+#     }
+#   ]
+#   regular_columns = [
+#     {
+#       column_name : "firstName"
+#       column_type : "text"
+#     },
+#     {
+#       column_name : "lastName"
+#       column_type : "text"
+#     },
+#     {
+#       column_name : "email"
+#       column_type : "text"
+#     }
+#   ]
 
-  partition_key_columns = [
-    {
-      column_name: "uid"
-      column_type: "uuid"
-    }
-  ]
-# regular_columns = [
-#   {
-#     column_name: "firstName"
-#     column_type: "text"
-#   },
-#   {
-#     column_name: "lastName"
-#     column_type: "text"
-#   },
-#   {
-#     column_name: "email"
-#     column_type: "text"
+#   encryption_specification = {
+#     encryption_type : "AWS_OWNED_KMS_KEY"
+#     kms_key_identifier : aws_kms_key.terraform.key_id
 #   }
-# ]
-
-  encryption_specification = {
-    encryption_type: "AWS_OWNED_KMS_KEY"
-    kms_key_identifier: aws_kms_key.example.key_id // # key ID or ARN
-  }
-}
+# }
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
 output "kms_key_id" {
   description = "ID of key used to encrypt table data."
-  value = aws_kms_key.example.key_id
+  value       = aws_kms_key.terraform.key_id
+}
+
+output "aws_region" {
+  description = "AWS region for all resources."
+  value       = var.aws_region
 }
